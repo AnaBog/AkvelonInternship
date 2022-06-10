ac4d06335310ff708b908bda3a586ec396dcd18f
Ana Bogdanovic <anabogdanovic995@gmail.com>
2022-06-10 10:45:27 +0200

    added interface

---
+++ AkvelonInternshipTask3_Interface/AkvelonInternshipTask3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkvelonInternshipTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Reflector reflector = new Reflector();
+            Refractor refractor = new Refractor();
+            reflector.ChangeType(refractor);
+
+            Console.WriteLine(reflector.ToString());
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Reflector : ITelescope
+    {
+        public void ChangeType<Refractor>(Refractor refractor)
+        {
+            Console.WriteLine(refractor);
+        }
+
+    }
+
+    public class Refractor : ITelescope
+    {
+        public void ChangeType<Reflector>(Reflector reflector)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
